e427f507d10f0b988e5b30d85cfee58f299e03c0
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-17 23:01:24 -0300

    feat: atualiza ConsultaRequestDTO com novos campos e formatação de data

--- src/main/java/br/com/cdb/agendadorDeConsultas/dto/ConsultaRequestDTO.java
+++ src/main/java/br/com/cdb/agendadorDeConsultas/dto/ConsultaRequestDTO.java
@@ -1,6 +1,11 @@
 package br.com.cdb.agendadorDeConsultas.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 
-public record ConsultaRequestDTO(String doctorName, String patientName, String PatientNumber, String title, String description, Date dataConsulta) {
+public record ConsultaRequestDTO(String doctorName, String patientName, String patientNumber, String speciality, String description,
+                                 @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss", shape = JsonFormat.Shape.STRING)
+                                 LocalDateTime consultationDateTime) {
 }
