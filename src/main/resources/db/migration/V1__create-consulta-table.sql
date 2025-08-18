5feeb1a8a76385134f92a18bdc58e8ed91e37abf
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-17 23:00:42 -0300

    feat: atualiza tabela de consulta com novos campos e renomeia existentes

--- src/main/resources/db/migration/V1__create-consulta-table.sql
+++ src/main/resources/db/migration/V1__create-consulta-table.sql
@@ -4,8 +4,8 @@ CREATE TABLE consulta (
     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     doctorName VARCHAR(100) NOT NULL,
     patientName VARCHAR(100) NOT NULL,
-    PatientNumber VARCHAR(20) NOT NULL,
-    title VARCHAR(100) NOT NULL,
+    patientNumber VARCHAR(20) NOT NULL,
+    speciality VARCHAR(100) NOT NULL,
     description VARCHAR(300) NOT NULL,
-    date TIMESTAMP NOT NULL
+    consultationDateTime TIMESTAMP NOT NULL
 );
