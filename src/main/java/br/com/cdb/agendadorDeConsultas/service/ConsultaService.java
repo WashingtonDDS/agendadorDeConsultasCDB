cf2a7018ce89344575c9b36510914c59626256fb
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-17 23:01:48 -0300

    feat: adiciona repositório de consultas e atualiza método de criação

--- src/main/java/br/com/cdb/agendadorDeConsultas/service/ConsultaService.java
+++ src/main/java/br/com/cdb/agendadorDeConsultas/service/ConsultaService.java
@@ -2,20 +2,27 @@ package br.com.cdb.agendadorDeConsultas.service;
 
 import br.com.cdb.agendadorDeConsultas.dto.ConsultaRequestDTO;
 import br.com.cdb.agendadorDeConsultas.entity.Consulta;
+import br.com.cdb.agendadorDeConsultas.repositories.ConsultaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-class ConsultaService {
+public class ConsultaService {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
 
     public Consulta createConsulta(ConsultaRequestDTO data){
 
         Consulta newConsulta = new Consulta();
         newConsulta.setDoctorName(data.doctorName());
         newConsulta.setPatientName(data.patientName());
-        newConsulta.setPatientNumber(data.PatientNumber());
-        newConsulta.setTitle(data.title());
+        newConsulta.setpatientNumber(data.patientNumber());
+        newConsulta.setSpeciality(data.speciality());
         newConsulta.setDescription(data.description());
-        newConsulta.setDataConsulta(data.dataConsulta());
+        newConsulta.setConsultationDateTime(data.consultationDateTime());
+
+        consultaRepository.save(newConsulta);
 
         return newConsulta;
     }
