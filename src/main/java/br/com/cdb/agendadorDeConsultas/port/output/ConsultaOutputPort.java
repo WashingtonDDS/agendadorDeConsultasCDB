e94fd862ec7ab31d005032bc7d86635af15d1bb7
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-23 17:09:03 -0300

    feat: adiciona métodos de busca e exclusão à interface ConsultaOutputPort

--- src/main/java/br/com/cdb/agendadorDeConsultas/port/output/ConsultaOutputPort.java
+++ src/main/java/br/com/cdb/agendadorDeConsultas/port/output/ConsultaOutputPort.java
@@ -2,6 +2,15 @@ package br.com.cdb.agendadorDeConsultas.port.output;
 
 import br.com.cdb.agendadorDeConsultas.core.domain.model.Consulta;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ConsultaOutputPort {
     Consulta save(Consulta consulta);
+    List<Consulta> findAll();
+    List<Consulta> findUpcomingConsultas(LocalDateTime now);
+    Optional<Consulta> findById(UUID id);
+    void delete(Consulta consulta);
 }
