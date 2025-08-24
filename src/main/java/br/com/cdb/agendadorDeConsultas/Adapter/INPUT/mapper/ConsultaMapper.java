b682be9a8b5c429755fd7ec5b8b7b7c23ccf70cf
WashingtonDDS <washington.DDS@hotmail.com>
2025-08-23 21:27:15 -0300

    feat: adiciona mapeamentos para conversão entre DTOs e modelo de domínio na classe ConsultaMapper

--- src/main/java/br/com/cdb/agendadorDeConsultas/Adapter/INPUT/mapper/ConsultaMapper.java
+++ src/main/java/br/com/cdb/agendadorDeConsultas/Adapter/INPUT/mapper/ConsultaMapper.java
@@ -1,4 +1,24 @@
 package br.com.cdb.agendadorDeConsultas.adapter.input.mapper;
 
+import br.com.cdb.agendadorDeConsultas.adapter.input.request.ConsultaDetailsDTO;
+import br.com.cdb.agendadorDeConsultas.adapter.input.request.ConsultaRequestDTO;
+import br.com.cdb.agendadorDeConsultas.adapter.input.request.ConsultaUpdateDTO;
+import br.com.cdb.agendadorDeConsultas.core.domain.model.Consulta;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.factory.Mappers;
+
+@Mapper(componentModel = "spring")
 public interface ConsultaMapper {
+    ConsultaMapper INSTANCE = Mappers.getMapper(ConsultaMapper.class);
+
+    Consulta toDomain(ConsultaRequestDTO request);
+
+    Consulta toDomain(ConsultaUpdateDTO request);
+
+    ConsultaRequestDTO toResponse(Consulta consulta);
+
+    ConsultaDetailsDTO toDetails(Consulta consulta);
+
+    void updateDomainFromDto(ConsultaUpdateDTO request,@MappingTarget Consulta consulta);
 }
