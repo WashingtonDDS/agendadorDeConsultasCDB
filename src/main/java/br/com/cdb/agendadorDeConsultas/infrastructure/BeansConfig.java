15010bae1bcb9100dd639358170039dec954ea80
WashingtonDDS <washington.DDS@hotmail.com>
2025-09-12 18:21:50 -0300

    feat(security): adicionar suporte a criptografia de senhas

--- src/main/java/br/com/cdb/agendadorDeConsultas/infrastructure/BeansConfig.java
+++ src/main/java/br/com/cdb/agendadorDeConsultas/infrastructure/BeansConfig.java
@@ -4,9 +4,16 @@ import br.com.cdb.agendadorDeConsultas.core.usecase.ConsultaUseCase;
 import br.com.cdb.agendadorDeConsultas.port.output.ConsultaOutputPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class BeansConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public ConsultaUseCase consultaUseCaseImpl(ConsultaOutputPort consultaOutputPort){
