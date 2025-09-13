76b96a73fa39c006eeec8e318966ace463638d60
WashingtonDDS <washington.DDS@hotmail.com>
2025-09-12 21:58:41 -0300

    feat(secretaria): implementar caso de uso para gerenciamento de secretarias

---
+++ src/main/java/br/com/cdb/agendadorDeConsultas/CORE/usecase/SecretariaUseCase.java
@@ -0,0 +1,59 @@
+package br.com.cdb.agendadorDeConsultas.core.usecase;
+
+import br.com.cdb.agendadorDeConsultas.adapter.input.request.SecretariaUpdate;
+import br.com.cdb.agendadorDeConsultas.core.domain.model.Secretaria;
+import br.com.cdb.agendadorDeConsultas.core.usecase.validation.SecretariaValidator;
+import br.com.cdb.agendadorDeConsultas.port.input.SecretariaInputPort;
+import br.com.cdb.agendadorDeConsultas.port.output.SecretariaOutputPort;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+import java.util.UUID;
+
+public class SecretariaUseCase implements SecretariaInputPort {
+
+    private final SecretariaOutputPort secretariaOutputPort;
+    private final PasswordEncoder passwordEncoder;
+    private final SecretariaValidator validator;
+
+
+    public SecretariaUseCase(SecretariaOutputPort secretariaOutputPort, PasswordEncoder passwordEncoder, SecretariaValidator validator) {
+        this.secretariaOutputPort = secretariaOutputPort;
+        this.passwordEncoder = passwordEncoder;
+        this.validator = validator;
+    }
+
+    @Override
+    public Secretaria create(Secretaria secretaria) {
+        secretaria.setPassword(passwordEncoder.encode(secretaria.getPassword()));
+        return secretariaOutputPort.save(secretaria);
+    }
+
+    @Override
+    public List<Secretaria> findAll() {
+        return secretariaOutputPort.findAll();
+    }
+
+    @Override
+    public Secretaria findById(UUID id) {
+        return secretariaOutputPort.findById(id);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        Secretaria secretaria = secretariaOutputPort.findById(id);
+        secretariaOutputPort.delete(secretaria);
+    }
+
+    @Override
+    public Secretaria update(UUID id, SecretariaUpdate secretariaUpdate) {
+        Secretaria secretaria = secretariaOutputPort.findById(id);
+
+        validator.validate(secretaria, secretariaUpdate);
+
+        secretaria.setName(secretariaUpdate.name());
+        secretaria.setEmail(secretariaUpdate.email());
+        secretaria.setPassword(passwordEncoder.encode(secretariaUpdate.password()));
+        return secretariaOutputPort.save(secretaria);
+    }
+}
