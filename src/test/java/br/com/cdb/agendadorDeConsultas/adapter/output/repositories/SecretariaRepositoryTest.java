c2f18f9622d08ed1007c8dae1d7a5909c5185b6b
WashingtonDDS <washington.DDS@hotmail.com>
2025-09-23 19:13:03 -0300

    test(repository): Adiciona classe de teste para ConsultaRepository  Adiciona classe de teste para SecretariaRepository

---
+++ src/test/java/br/com/cdb/agendadorDeConsultas/adapter/output/repositories/SecretariaRepositoryTest.java
@@ -0,0 +1,28 @@
+package br.com.cdb.agendadorDeConsultas.adapter.output.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SecretariaRepositoryTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+}
